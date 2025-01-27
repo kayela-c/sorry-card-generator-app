8d878dd898ca4990c7ac02255e9323c9327eaa0c
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2025-01-27 13:38:21 -0600

    es lint drizzle

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,7 @@ const config = {
     "project": true
   },
   "plugins": [
-    "@typescript-eslint",
-    "drizzle"
+    "@typescript-eslint"
   ],
   "extends": [
     "next/core-web-vitals",
@@ -38,24 +37,8 @@ const config = {
         }
       }
     ],
-    "drizzle/enforce-delete-with-where": [
-      "error",
-      {
-        "drizzleObjectName": [
-          "db",
-          "ctx.db"
-        ]
-      }
-    ],
-    "drizzle/enforce-update-with-where": [
-      "error",
-      {
-        "drizzleObjectName": [
-          "db",
-          "ctx.db"
-        ]
-      }
-    ]
+  
+
   }
 }
 module.exports = config;
