bdb67a23469e59384efeeaa22745bfd317c2c1b3
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2025-01-30 16:49:51 -0600

    removed unused redunant code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import ExpCardDeck from "@/components/ui/ExpCardDeck";
+import DeckUI from "@/components/ui/DeckUi";
 import React from "react";
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-700 text-black">
-      <ExpCardDeck />
+      <DeckUI />
     </main>
   );
 }
--- src/components/ui/ExpCardDeck.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DeckUI from "./DeckUi";
-
-const ExpCardDeck = () => {
-  return <DeckUI />;
-};
-
-export default ExpCardDeck;
