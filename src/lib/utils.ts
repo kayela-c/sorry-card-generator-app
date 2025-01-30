617f304333829b09f6451cd9973d249ad7b10885
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2025-01-30 16:51:13 -0600

    notes on unused code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+//not being used in this project. Left here for future reference
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
