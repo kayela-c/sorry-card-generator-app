56b4b289c83be10a2d5ca28d9ede72134a1dbcff
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2025-02-05 15:29:20 -0600

    deleted unnecessary notes

--- src/hooks/RandomIndex.ts
+++ src/hooks/RandomIndex.ts
@@ -20,12 +20,3 @@ export function getDataSlice(data: Data, levelDifficulty: number) {
   return dataSlice;
 }
 
-/**
- * Challenge:
- * 1) Below the "startGame" function, create a new function called "getDataSlice". The function should receive "data" as a parameter.
- * 2) Inside this function, make a call to "getRandomIndex" and store the return value in a variable called "randomIndex".
- * 3) Map over "randomIndex" and use the random numbers stored in this array to create a new array of random emojis selected from "data". Store this new array in a variable called "dataSlice" and return it at the bottom of the function.
- * 4) Inside the try block of the "startGame" function, make a call to "getDataSlice", passing "data" as an argument. Save the return value in a variable called "dataSlice".
- * 5) Delete the "dataSample" variable and replace "dataSample" with the new "dataSlice" variable in the "setEmojisData" function.
- * 6) Run the code and start a new game to check that your code is working.
- */
